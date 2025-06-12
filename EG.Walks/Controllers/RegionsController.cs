47063169efabd29f4fee0aba08d1c944b64929cc
BeRo <beroooberooo6@gmail.com>
2025-06-12 16:49:35 +0300

    Add GetAll Regions endpoint

---
+++ EG.Walks/Controllers/RegionsController.cs
@@ -0,0 +1,27 @@
+﻿using EG.Walks.Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EG.Walks.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly EGWalksDbContext _dbContext;
+        public RegionsController(EGWalksDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var reagions = _dbContext.Regions.ToList();
+            if (reagions == null || !reagions.Any())
+            {
+                return NotFound("No regions found.");
+            }
+            return Ok(reagions);
+        }
+    }
+}
