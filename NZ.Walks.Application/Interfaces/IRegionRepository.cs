dce92c22d01e8d9bd1ef076560d47d3dea4626f8
BeRo <beroooberooo6@gmail.com>
2025-06-17 18:45:42 +0300

    Restructure folders to follow Clean Architecture

--- NZ.Walks.Application/Interfaces/IRegionRepository.cs
+++ NZ.Walks.Application/Interfaces/IRegionRepository.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using EG.Walks.Domain.Entities;
-using Microsoft.AspNetCore.Mvc;
 
 namespace EG.Walks.Infrastructure.Repository.IRepository
 {
