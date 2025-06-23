c41e30d434570b5a63abec599046d8bca0276bb5
Alber Ashraf <102770811+Alber-Ashraf@users.noreply.github.com>
2025-06-23 18:26:58 +0300

    Delete NZ.Walks.Application directory

--- NZ.Walks.Application/Interfaces/ITokenRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace EG.Walks.Application.Interfaces
-{
-    public interface ITokenRepository
-    {
-        string CreateJWTToken(IdentityUser user, List<string> roles);
-    }
-}
