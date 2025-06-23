0d185ffb17dee8b1f48e8e9f25da05c9a77ddf89
BeRo <beroooberooo6@gmail.com>
2025-06-23 15:40:34 +0300

    Consume Web API Implement Add new Walk

--- EG.Walks.UI/Controllers/WalksController.cs
+++ EG.Walks.UI/Controllers/WalksController.cs
@@ -1,6 +1,8 @@
 ﻿using EG.Walks.UI.Models;
 using EG.Walks.UI.Models.DTOs;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Text;
 
 namespace EG.Walks.UI.Controllers
 {
@@ -12,42 +14,62 @@ namespace EG.Walks.UI.Controllers
             _httpClientFactory = httpClientFactory;
         }
 
+        [HttpGet]
         public async Task<IActionResult> GetAll(string? filterOn, string? filterQuery,
         string? sortBy, bool isAscending = true, int page = 1, int pageSize = 10)
-{
-    try
-    {
-        var client = _httpClientFactory.CreateClient();
-
-        // Construct query parameters
-        var query = $"?filterOn={filterOn}&filterQuery={filterQuery}&sortBy={sortBy}&isAscending={isAscending}&page={page}&pageSize={pageSize}";
-        var response = await client.GetAsync($"https://localhost:7013/api/Walks{query}");
-
-        if (!response.IsSuccessStatusCode)
-            return View("Error");
-
-        var walks = await response.Content.ReadFromJsonAsync<List<WalkResponseDto>>();
-
-        // Create the ViewModel
-        var viewModel = new WalkFilterViewModel
         {
-            Walks = walks,
-            FilterOn = filterOn,
-            FilterQuery = filterQuery,
-            SortBy = sortBy,
-            IsAscending = isAscending,
-            Page = page,
-            PageSize = pageSize,
-            TotalPages = 10
-        };
+            var client = _httpClientFactory.CreateClient();
 
-        return View(viewModel);
-    }
-    catch
-    {
-        return View("Error");
-    }
-}
+            // Construct query parameters
+            var query = $"?filterOn={filterOn}&filterQuery={filterQuery}&sortBy={sortBy}&isAscending={isAscending}&page={page}&pageSize={pageSize}";
+            var response = await client.GetAsync($"https://localhost:7013/api/Walks{query}");
 
+            if (!response.IsSuccessStatusCode)
+                return View("Error");
+
+            var walks = await response.Content.ReadFromJsonAsync<List<WalkResponseDto>>();
+
+            // Create the ViewModel
+            var viewModel = new WalkFilterViewModel
+            {
+                Walks = walks,
+                FilterOn = filterOn,
+                FilterQuery = filterQuery,
+                SortBy = sortBy,
+                IsAscending = isAscending,
+                Page = page,
+                PageSize = pageSize,
+                TotalPages = 10
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddWalkViewModel model)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var httpRequestMassage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("https://localhost:7013/api/Walks"),
+                Content = new StringContent(JsonSerializer.Serialize(model), Encoding.UTF8, "application/json")
+            };
+            var httpResponseMassage = await client.SendAsync(httpRequestMassage);
+            httpResponseMassage.EnsureSuccessStatusCode();
+
+            var responseContent = await httpResponseMassage.Content.ReadFromJsonAsync<WalkResponseDto>();
+            if (responseContent is not null)
+            {
+                return RedirectToAction(nameof(GetAll));
+            }
+            return View();
+        }
     }
 }
