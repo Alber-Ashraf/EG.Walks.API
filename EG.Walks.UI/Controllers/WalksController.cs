66aa13518250467ad5daddf38c4778af4d712fc7
BeRo <beroooberooo6@gmail.com>
2025-06-23 14:20:58 +0300

    Consume Web API Implement GettAll Walks

---
+++ EG.Walks.UI/Controllers/WalksController.cs
@@ -0,0 +1,53 @@
+﻿using EG.Walks.UI.Models;
+using EG.Walks.UI.Models.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EG.Walks.UI.Controllers
+{
+    public class WalksController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public WalksController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> GetAll(string? filterOn, string? filterQuery,
+        string? sortBy, bool isAscending = true, int page = 1, int pageSize = 10)
+{
+    try
+    {
+        var client = _httpClientFactory.CreateClient();
+
+        // Construct query parameters
+        var query = $"?filterOn={filterOn}&filterQuery={filterQuery}&sortBy={sortBy}&isAscending={isAscending}&page={page}&pageSize={pageSize}";
+        var response = await client.GetAsync($"https://localhost:7013/api/Walks{query}");
+
+        if (!response.IsSuccessStatusCode)
+            return View("Error");
+
+        var walks = await response.Content.ReadFromJsonAsync<List<WalkResponseDto>>();
+
+        // Create the ViewModel
+        var viewModel = new WalkFilterViewModel
+        {
+            Walks = walks,
+            FilterOn = filterOn,
+            FilterQuery = filterQuery,
+            SortBy = sortBy,
+            IsAscending = isAscending,
+            Page = page,
+            PageSize = pageSize,
+            TotalPages = 10
+        };
+
+        return View(viewModel);
+    }
+    catch
+    {
+        return View("Error");
+    }
+}
+
+    }
+}
---
+++ EG.Walks.UI/Models/WalkFilterViewModel.cs
@@ -0,0 +1,21 @@
+﻿using EG.Walks.UI.Models.DTOs;
+
+namespace EG.Walks.UI.Models
+{
+    public class WalkFilterViewModel
+    {
+        public List<WalkResponseDto> Walks { get; set; } = new List<WalkResponseDto>();
+
+        // Filter/Sort/Paging Parameters
+        public string? FilterOn { get; set; }
+        public string? FilterQuery { get; set; }
+        public string? SortBy { get; set; }
+        public bool IsAscending { get; set; } = true;
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+
+        // For pagination UI
+        public int TotalPages { get; set; }
+    }
+
+}
